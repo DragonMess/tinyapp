6b1045164c3941c21e8d7db44ade6f6a80f854d8
DragonMess <kmylop@hotmail.com>
2020-01-20 23:34:59 +0000

    Header add

--- express_server.js
+++ express_server.js
@@ -2,15 +2,33 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port
 
+app.set("view engine", "ejs");
+
 // get a web page on path url/hello (in html)
 app.get("/hello", (req, res) => {
   res.send("<html><body> Hello <b>World</b></body></html>\n");
 });
 
-const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+
+//=========== ejs ===================
+
+// add route /urls & pass data with res.render
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  let templateVars = { greeting: 'Hello World'};
+  res.render('hello_world', templateVars);
+});
+app.get("/urls/:shortURL", (req, res) => {
+  let templateVars = { shortURL: req.params.shortURL, longURL: "http://www.lighthouselabs.ca" };
+  res.render("urls_show", templateVars);
+});
+
+
+//================GET==============
 
 // get a web with a "hello" in body
 app.get("/", (req, res) => {
@@ -24,7 +42,13 @@ app.get("/set", (req, res) => {
 app.get("/fetch", (req, res) => {
   res.send(`a = ${a}`);
 })
+//================================
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
