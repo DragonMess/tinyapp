1f34c5f33787127a7cf7ea7a3ec5614c663041aa
DragonMess <kmylop@hotmail.com>
2020-01-20 20:10:09 +0000

    practice with get

--- express_server.js
+++ express_server.js
@@ -2,14 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = 8080; //default port
 
+// get a web page on path url/hello (in html)
+app.get("/hello", (req, res) => {
+  res.send("<html><body> Hello <b>World</b></body></html>\n");
+});
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+// get a web with a "hello" in body
 app.get("/", (req, res) => {
   res.send("Hello");
 });
+
+app.get("/set", (req, res) => {
+  const a =1;
+  res.send(`a = ${a}`);
+})
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
